aa840b7a16b3039e1274d76a939e566c0095eddf
Hack Reactor Students <anthony.zavadil@gmail.com>
2014-06-18 15:46:32 -0700

    "Songs collection completed"

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,13 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
-  model: SongModel
+  model: SongModel,
+
+  initialize: function(songDataArray){
+    for(var i = 0; i < songDataArray.length; i++ ){
+      var curData = songDataArray[i];
+      this.collection.add(new SongModel(curData));
+    }
+  }
 
 });
